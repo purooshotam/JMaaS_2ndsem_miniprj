d07403bd57a56a632b9a163b7f26c82d12c3714e
purooshotam <purooshotamkumar0005@gmail.com>
2026-07-26 21:51:47 +0530

    Add individual API endpoints for Vercel

---
+++ JMaaS_2ndsem_miniprj-main/api/store-info.js
@@ -0,0 +1,16 @@
+const { storeInfo } = require('../data');
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  res.json({
+    success: true,
+    data: storeInfo
+  });
+};
